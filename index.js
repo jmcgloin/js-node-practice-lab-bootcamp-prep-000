e65fb011cb3ba4454e5a5a4ec35b791a11913ab2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 01:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Jason"
 var height = "77"
 
-console.log("Name:")
-console.log(name)
+console.log(`Name: ${name}`)
+//console.log(name)
 
-console.log("Height:")
+console.log(`Height: ${height}`)
 console.log(height)
